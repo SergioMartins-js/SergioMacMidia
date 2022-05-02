7ef6692ddbd116d0c8154a907c3038073decc466
SergioMartins-JS <sergiomartinscassimiro@gmail.com>
2022-05-01 21:34:13 -0300

    weblinks 2

---
+++ page.js
@@ -0,0 +1,72 @@
+var n1 = prompt("digite um número com dd no formato (61998285445)");
+
+var link =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%20somos%20de%20uma%20empresa%20de%20marketing%20especializada%20em%20cria%C3%A7%C3%A3o%20de%20sites%20de%20visitas%2C%20o%20que%20acha%20de%20impulsionar%20sua%20marca%20na%20rede%20mais%20conhecida%20do%20mundo%3F";
+
+function enviar() {
+  window.location.assign(link);
+}
+
+var link1 =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%2C%20fazemos%20parte%20de%20uma%20empresa%20voltada%20para%20o%20desenvolvimento%20web%20do%20seu%20neg%C3%B3cio%2C%20o%20que%20acha%20de%20criar%20o%20seu%20site%20a%20um%20baixo%20custo%20e%20alavancar%20sua%20empresa%20na%20rede%20mais%20conhecida%20do%20mundo%3F%20";
+
+function enviar1() {
+  window.location.assign(link1);
+}
+
+var link2 =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%2C%20Somos%20de%20uma%20empresa%20voltada%20para%20o%20desenvolvimento%20web%20do%20seu%20neg%C3%B3cio%2C%20o%20que%20acha%20de%20fazer%20parte%20da%20rede%20mais%20conhecida%20do%20mundo%2C%20e%20se%20tornar%20parte%20do%20Google%3F";
+function enviar2() {
+  window.location.assign(link2);
+}
+
+var link3 =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%2C%20fazemos%20parte%20de%20uma%20empresa%20voltada%20para%20o%20desenvolvimento%20web%20do%20seu%20neg%C3%B3cio%2C%20o%20que%20acha%20de%20criar%20o%20seu%20site%20a%20um%20baixo%20custo%20e%20alavancar%20sua%20empresa%20na%20rede%20mais%20conhecida%20do%20mundo%3F%20";
+
+function enviar3() {
+  window.location.assign(link3);
+}
+
+var link4 =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%2C%20fazemos%20parte%20de%20uma%20empresa%20voltada%20para%20o%20desenvolvimento%20web%20do%20seu%20neg%C3%B3cio%2C%20o%20que%20acha%20de%20criar%20o%20seu%20site%20a%20um%20baixo%20custo%20e%20alavancar%20sua%20empresa%20na%20rede%20mais%20conhecida%20do%20mundo%3F%20";
+
+function enviar4() {
+  window.location.assign(link4);
+}
+
+var link5 =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%2C%20fazemos%20parte%20de%20uma%20empresa%20voltada%20para%20o%20desenvolvimento%20web%20do%20seu%20neg%C3%B3cio%2C%20o%20que%20acha%20de%20criar%20o%20seu%20site%20a%20um%20baixo%20custo%20e%20alavancar%20sua%20empresa%20na%20rede%20mais%20conhecida%20do%20mundo%3F%20";
+
+function enviar5() {
+  window.location.assign(link5);
+}
+
+var link6 =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%2C%20fazemos%20parte%20de%20uma%20empresa%20voltada%20para%20o%20desenvolvimento%20web%20do%20seu%20neg%C3%B3cio%2C%20o%20que%20acha%20de%20criar%20o%20seu%20site%20a%20um%20baixo%20custo%20e%20alavancar%20sua%20empresa%20na%20rede%20mais%20conhecida%20do%20mundo%3F%20";
+
+function enviar6() {
+  window.location.assign(link6);
+}
+
+var link7 =
+  "https://api.whatsapp.com/send?phone=55" +
+  n1 +
+  "&text=Ol%C3%A1%2C%20fazemos%20parte%20de%20uma%20empresa%20voltada%20para%20o%20desenvolvimento%20web%20do%20seu%20neg%C3%B3cio%2C%20o%20que%20acha%20de%20criar%20o%20seu%20site%20a%20um%20baixo%20custo%20e%20alavancar%20sua%20empresa%20na%20rede%20mais%20conhecida%20do%20mundo%3F%20";
+
+function enviar7() {
+  window.location.assign(link7);
+}
